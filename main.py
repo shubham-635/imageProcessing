b726ab0a3a9b28c84dd31b0aafddc422355cf966
shubham-635 <95330626+shubham-635@users.noreply.github.com>
2024-07-10 10:51:12 +0530

    Add Docstrings and Comments

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 from typing import List
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
-import aiohttp, aiofiles
+import aiohttp
+import aiofiles
 import io
 from PIL import Image
 from fastapi import FastAPI, HTTPException, UploadFile, BackgroundTasks
@@ -16,29 +17,33 @@ import uvicorn
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv(".env")
 
+# Initialize FastAPI app
 app = FastAPI()
 
+# Database connection details
 db_url = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(db_url)
 db = client.process_image
 requests_coll = db["requests"]
 items_coll = db["items"]
 
-
+# Response model for upload endpoint
 class UploadResponse(BaseModel):
     request_id: str
 
-
+# Response model for status endpoint
 class StatusResponse(BaseModel):
     request_id: str
     status: str
     compressed_images: list
 
-
 class AWSS3:
-
+    """
+    Class to handle AWS S3 interactions
+    """
     def __init__(self) -> None:
         self.S3_BUCKET = os.getenv("S3_BUCKET")
         self.S3_KEY = os.getenv("S3_KEY")
@@ -47,18 +52,25 @@ class AWSS3:
         self.REGION_NAME = os.getenv('REGION_NAME')
 
     def upload_file_aws(self, byte_data, file_name):
+        """
+        Uploads a file to AWS S3 and returns the file URL
+        """
         try:
-            client = boto3.client('s3', region_name=self.REGION_NAME, aws_access_key_id=self.S3_KEY, aws_secret_access_key=self.S3_SECRET)
+            client = boto3.client('s3', region_name=self.REGION_NAME, 
+                                  aws_access_key_id=self.S3_KEY, 
+                                  aws_secret_access_key=self.S3_SECRET)
             
             fields = {'acl': 'public-read'}
             conditions = [{"acl": "public-read"}]
-
             fields['Content-Type'] = "image/png"
             conditions.append({"Content-Type": "image/png"})
-                
+            
             buc_address = "Bucket Address"
-            boto3_response = client.generate_presigned_post(Bucket=self.S3_BUCKET, Key=buc_address, Fields=fields,
-                                                            ExpiresIn=60, Conditions=conditions)
+            boto3_response = client.generate_presigned_post(Bucket=self.S3_BUCKET, 
+                                                            Key=buc_address, 
+                                                            Fields=fields, 
+                                                            ExpiresIn=60, 
+                                                            Conditions=conditions)
             
             files = {'file': (file_name, byte_data)}
             data = boto3_response['fields']
@@ -72,8 +84,10 @@ class AWSS3:
         except Exception as e:
             return ""
 
-
 async def compress_image(url: str) -> bytes:
+    """
+    Downloads an image from a URL, compresses it to 50% quality, and returns the compressed image bytes
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url) as response:
             if response.status == 200:
@@ -84,35 +98,44 @@ async def compress_image(url: str) -> bytes:
                 buf.seek(0)
                 return buf.read()
 
-
 async def process_image(request_id: str, items_list: List[str]):
+    """
+    Processes the images for a given request by compressing them and storing the results in the database
+    """
     for item in items_list:
         comp_img_urls = []
         for url in item["item_urls"]:
             comp_img = await compress_image(url)
             comp_img_url = f"/processed_img/{uuid4()}.jpeg"
 
-            ## Using S3 Bucket to generate publicly accessable URL (Optional)
+            # Using S3 Bucket to generate publicly accessible URL (Optional)
             comp_img_url = AWSS3().upload_file_aws(comp_img, comp_img_url)
             comp_img_urls.append(comp_img_url)
-        await items_coll.insert_one({"_id": str(uuid4()),
-                                     "request_id": request_id,
-                                     "created_at": time.time(),
-                                     "updated_at": time.time(),
-                                     "input_urls": item["item_urls"],
-                                     "serial_no": item["serial_no"],
-                                     "item_name": item["item_name"],
-                                     "output_urls": comp_img_urls})
         
+        # Insert processed image data into the database
+        await items_coll.insert_one({
+            "_id": str(uuid4()),
+            "request_id": request_id,
+            "created_at": time.time(),
+            "updated_at": time.time(),
+            "input_urls": item["item_urls"],
+            "serial_no": item["serial_no"],
+            "item_name": item["item_name"],
+            "output_urls": comp_img_urls
+        })
+    
+    # Update the request status to "Completed"
     await requests_coll.update_one({"_id": request_id}, {"$set": {"status": "Completed", "updated_at": time.time()}})
 
-
-@app.post("/upload", response_model = UploadResponse)
-async def uplooad_csv_api(csv_file: UploadFile, background_task: BackgroundTasks) -> JSONResponse:
-    file_content = csv_file.read()
-
+@app.post("/upload", response_model=UploadResponse)
+async def upload_csv_api(csv_file: UploadFile, background_task: BackgroundTasks) -> JSONResponse:
+    """
+    API endpoint to upload a CSV file and start the background task for image processing
+    """
+    file_content = await csv_file.read()
     request_id = str(uuid4())
 
+    # Insert initial request data into the database
     await requests_coll.insert_one({
         "_id": request_id,
         "status": "Pending",
@@ -120,37 +143,40 @@ async def uplooad_csv_api(csv_file: UploadFile, background_task: BackgroundTasks
         "updated_at": time.time()
     })
 
+    # Save the uploaded CSV file to disk
     async with aiofiles.open(f"{request_id}.csv", "wb") as csv_file:
         await csv_file.write(file_content)
 
     items_list = []
 
+    # Read and parse the CSV file
     async with aiofiles.open(f"{request_id}.csv", "r") as csv_file:
         content_dict = csv.DictReader(csv_file)
         for row in content_dict:
             items_list.append({
                 "serial_no": row.get("S. No."),
-                # "request_id": request_id,
                 "item_name": row.get("Product Name"),
                 "item_urls": row.get("Input Image Urls", "").split(",")
             })
-        # await items_coll.insert_many(items_list)
 
-    await requests_coll.update_one({"_id": request_id}, {"$set": {"status": "Processing",
-                                                                  "updated_at": time.time()}})
+    # Update the request status to "Processing"
+    await requests_coll.update_one({"_id": request_id}, {"$set": {"status": "Processing", "updated_at": time.time()}})
 
+    # Add the image processing task to the background tasks
     background_task.add_task(process_image, request_id, items_list)
+    
     return JSONResponse({"request_id": request_id}, status_code=HTTP_200_OK)
-
 
 @app.get("/status/{request_id}", response_model=StatusResponse)
 async def get_image_processing_status_api(request_id: str) -> JSONResponse:
-    request_obj = requests_coll.find_one({"_id": request_id})
+    """
+    API endpoint to get the status of an image processing request by request ID
+    """
+    request_obj = await requests_coll.find_one({"_id": request_id})
     if not request_obj:
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Request not Found")
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Request not found")
     
     return JSONResponse({"request_id": request_id, "status": request_obj["status"]})
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
